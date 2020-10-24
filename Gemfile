a76af3b8a4f1eb33513e7322acd6a97b59695f4f
Yusuke Yonekura <42902626+bimadisiwin@users.noreply.github.com>
2020-10-24 12:54:41 +0900

    Make Memo App.

--- Gemfile
+++ Gemfile
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
-
-# gem "rails"
+gem 'activerecord'
+gem 'omniauth'
+gem 'rack-contrib'
 gem 'sinatra'
+gem 'sinatra-activerecord'
 gem 'sinatra-contrib'
+gem 'sqlite3'
+gem 'unicorn'
--- main.rb
+++ main.rb
@@ -1,18 +1,78 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
+require 'securerandom'
 
-get '/top' do
+json_file_path = 'memos/memo.json'
+
+json_data = open(json_file_path) { |io| JSON.load(io) }
+
+memos = json_data['memos']
+
+get '/' do
+  @memos = memos
   erb :top
-end
-
-get '/show' do
-  erb :show
 end
 
 get '/new' do
   erb :new
 end
 
-get '/edit' do
+get '/memo/:id' do
+  memos.each do |memo|
+    if memo['id'].to_s == params[:id].to_s
+      @title = memo['title'].to_s
+      @content = memo['content']
+    end
+  end
+  erb :show
+end
+
+post '/new' do
+  id = SecureRandom.uuid
+  new_memo = { 'id' => id.to_s, 'title' => params[:title], 'content' => params[:content] }
+  memos.push(new_memo)
+
+  File.open(json_file_path, 'w') do |file|
+    JSON.dump(json_data, file)
+  end
+  redirect '/'
+  erb :top
+end
+
+get '/memo/edit/:id' do
+  memos.each do |memo|
+    if memo['id'].to_s == params[:id].to_s
+      @title = memo['title'].to_s
+      @content = memo['content']
+    end
+  end
   erb :edit
+end
+
+patch '/memo/edit/:id' do
+  memos.each do |memo|
+    if memo['id'].to_s == params[:id].to_s
+      memo['title'] = params[:title]
+      memo['content'] = params[:content]
+    end
+  end
+  File.open(json_file_path, 'w') do |file|
+    JSON.dump(json_data, file)
+  end
+  redirect '/'
+  erb :top
+end
+
+delete '/memo/delete/:id' do
+  memos.each do |memo|
+    memos.delete(memo) if memo['id'].to_s == params[:id].to_s
+  end
+  File.open(json_file_path, 'w') do |file|
+    JSON.dump(json_data, file)
+  end
+  redirect '/'
+  erb :top
 end
