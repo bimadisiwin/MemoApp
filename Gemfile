79649ea4225c5d6469e58acb800a13fb9547ce02
Yusuke Yonekura <42902626+bimadisiwin@users.noreply.github.com>
2020-10-28 11:06:20 +0900

    Add XSS measures

--- Gemfile
+++ Gemfile
@@ -2,11 +2,5 @@
 
 source 'https://rubygems.org'
 
-gem 'activerecord'
-gem 'omniauth'
-gem 'rack-contrib'
 gem 'sinatra'
-gem 'sinatra-activerecord'
 gem 'sinatra-contrib'
-gem 'sqlite3'
-gem 'unicorn'
--- main.rb
+++ main.rb
@@ -5,11 +5,21 @@ require 'sinatra/reloader'
 require 'json'
 require 'securerandom'
 
-json_file_path = 'memos/memo.json'
+JSON_FILE_PATH = 'memos/memo.json'
+$json_data = open(JSON_FILE_PATH) { |io| JSON.load(io) }
+memos = $json_data['memos']
 
-json_data = open(json_file_path) { |io| JSON.load(io) }
+def write_json
+  File.open(JSON_FILE_PATH, 'w') do |file|
+    JSON.dump($json_data, file)
+  end
+end
 
-memos = json_data['memos']
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
 
 get '/' do
   @memos = memos
@@ -20,59 +30,52 @@ get '/new' do
   erb :new
 end
 
-get '/memo/:id' do
-  memos.each do |memo|
-    if memo['id'].to_s == params[:id].to_s
-      @title = memo['title'].to_s
+get '/memo/:id' do |id|
+  memos.find do |memo|
+    if memo['id'] == id
+      @title = memo['title']
       @content = memo['content']
     end
   end
   erb :show
 end
 
-post '/new' do
+post '/memo' do
   id = SecureRandom.uuid
-  new_memo = { 'id' => id.to_s, 'title' => params[:title], 'content' => params[:content] }
+  new_memo = {'id' => id.to_s, 'title' => "#{h(params[:title])}", 'content' => "#{h(params[:content])}"}
   memos.push(new_memo)
-
-  File.open(json_file_path, 'w') do |file|
-    JSON.dump(json_data, file)
-  end
+  write_json
   redirect '/'
   erb :top
 end
 
-get '/memo/edit/:id' do
-  memos.each do |memo|
-    if memo['id'].to_s == params[:id].to_s
-      @title = memo['title'].to_s
+get '/memo/:id/edit' do |id|
+  memos.find do |memo|
+    if memo['id'] == id
+      @title = memo['title']
       @content = memo['content']
     end
   end
   erb :edit
 end
 
-patch '/memo/edit/:id' do
+patch '/memo/:id' do |id|
   memos.each do |memo|
-    if memo['id'].to_s == params[:id].to_s
-      memo['title'] = params[:title]
-      memo['content'] = params[:content]
+    if memo['id'] == id
+      memo['title'] = "#{h(params[:title])}"
+      memo['content'] = "#{h(params[:content])}"
     end
   end
-  File.open(json_file_path, 'w') do |file|
-    JSON.dump(json_data, file)
-  end
+  write_json
   redirect '/'
   erb :top
 end
 
-delete '/memo/delete/:id' do
-  memos.each do |memo|
-    memos.delete(memo) if memo['id'].to_s == params[:id].to_s
+delete '/memo/:id' do |id|
+  memos.find do |memo|
+    memos.delete(memo) if memo['id'] == id
   end
-  File.open(json_file_path, 'w') do |file|
-    JSON.dump(json_data, file)
-  end
+  write_json
   redirect '/'
   erb :top
 end
