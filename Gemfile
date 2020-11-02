718e4e15393f12dc8f37a6bfb38ff8ae1b54846c
Yusuke Yonekura <42902626+bimadisiwin@users.noreply.github.com>
2020-11-03 00:15:21 +0900

    Add 404 Not Found

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 
 source 'https://rubygems.org'
 
+gem 'dotenv'
 gem 'pg'
 gem 'sinatra'
 gem 'sinatra-contrib'
-gem 'dotenv'
--- main.rb
+++ main.rb
@@ -11,15 +11,29 @@ helpers do
   end
 end
 
-connection = PG::connect(
-    host: ENV["DB_HOST"],
-    user: ENV["DB_USER"],
-    dbname: ENV["DB_NAME"]
-)
+not_found do
+  '404 Not Found.'
+end
+
+def connect_db
+  connection = PG.connect(
+    host: ENV['DB_HOST'],
+    user: ENV['DB_USER'],
+    dbname: ENV['DB_NAME']
+  )
+  begin
+    yield(connection) if block_given?
+  ensure
+    connection.finish
+  end
+end
 
 get '/' do
-  memos = connection.exec('SELECT * FROM memos;')
-  @memos = memos
+  connect_db do |connection|
+    memos = connection.exec('SELECT * FROM memos;')
+    @memos = memos
+  end
+
   erb :top
 end
 
@@ -28,37 +42,59 @@ get '/new' do
 end
 
 get '/memo/:id' do |id|
-  memo = connection.exec_params("SELECT * FROM memos WHERE id=$1;", [id])
-  @title = memo[0]["title"]
-  @content = memo[0]["content"]
+  begin
+    connect_db do |connection|
+      memo = connection.exec_params('SELECT * FROM memos WHERE id=$1;', [id])
+      @title = memo[0]['title']
+      @content = memo[0]['content']
+    end
+  rescue StandardError
+    not_found
+  end
   erb :show
 end
 
 post '/memo' do
   title = params[:title]
   content = params[:content]
-  connection.exec_params("INSERT INTO memos(title, content) VALUES ($1, $2);", [title, content])
+  connect_db do |connection|
+    connection.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', [title, content])
+  end
   redirect '/'
   erb :top
 end
 
 get '/memo/:id/edit' do |id|
-  memo = connection.exec_params("SELECT * FROM memos WHERE id=$1;", [id])
-  @title = memo[0]["title"]
-  @content = memo[0]["content"]
+  begin
+    connect_db do |connection|
+      memo = connection.exec_params('SELECT * FROM memos WHERE id=$1;', [id])
+      @title = memo[0]['title']
+      @content = memo[0]['content']
+    end
+  rescue StandardError
+    not_found
+  end
   erb :edit
 end
 
 patch '/memo/:id' do |id|
   title = params[:title]
   content = params[:content]
-  connection.exec_params("UPDATE memos SET title = $1, content = $2 WHERE id = $3;", [title, content, id])
+  connect_db do |connection|
+    connection.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3;', [title, content, id])
+  end
   redirect '/'
   erb :top
 end
 
 delete '/memo/:id' do |id|
-  connection.exec_params("DELETE FROM Memos WHERE id=$1;", [id])
+  begin
+    connect_db do |connection|
+      connection.exec_params('DELETE FROM Memos WHERE id=$1;', [id])
+    end
+  rescue StandardError
+    not_found
+  end
   redirect '/'
   erb :top
 end
